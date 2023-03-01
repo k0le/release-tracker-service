d9174b6d23dcf6a97f12bb8a79a6f420789cdd98
Vladimir Kolarevic <vlada.kole@gmail.com>
2023-03-01 16:49:22 +0100

    change from string tu UUID

--- release-tracker-domain/src/main/java/com/vladimirkolarevic/releasetracker/domain/Release.java
+++ release-tracker-domain/src/main/java/com/vladimirkolarevic/releasetracker/domain/Release.java
@@ -2,9 +2,10 @@ package com.vladimirkolarevic.releasetracker.domain;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
- public record Release(
-        String id,
+public record Release(
+        UUID id,
         String name,
         String description,
         ReleaseStatus status,
