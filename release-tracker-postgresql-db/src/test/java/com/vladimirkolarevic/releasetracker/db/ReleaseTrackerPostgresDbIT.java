ba44399837df922eb81ed36747f880b60956d6d3
Vladimir Kolarevic <vlada.kole@gmail.com>
2023-03-03 13:52:26 +0100

    Added persistent module

--- release-tracker-postgresql-db/src/test/java/com/vladimirkolarevic/releasetracker/db/ReleaseTrackerPostgresDbIT.java
+++ release-tracker-postgresql-db/src/test/java/com/vladimirkolarevic/releasetracker/db/ReleaseTrackerPostgresDbIT.java
@@ -1,10 +1,8 @@
 package com.vladimirkolarevic.releasetracker.db;
 
 import com.vladimirkolarevic.releasetracker.db.container.PostgreSQLContainerInitializer;
-import com.vladimirkolarevic.releasetracker.domain.Release;
 import com.vladimirkolarevic.releasetracker.domain.ReleaseService;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,9 +14,8 @@ import org.springframework.test.context.ContextConfiguration;
 public class ReleaseTrackerPostgresDbIT {
     @Autowired
     private ReleaseService releaseService;
-
-    @ParameterizedTest
-    void givenReleaseDone_returnSavedDoneRelease(){
+    @Test
+    void contextTest(){
 
     }
 }
